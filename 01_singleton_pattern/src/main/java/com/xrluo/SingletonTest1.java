45bc88b1d28ff01d56db91f6f148dcdcdd3018ff
闫循祯 <yxz4ever@sina.com>
2020-12-20 06:51:33 +0800

    单例模式(饿汉) 简单代码

--- 01_singleton_pattern/src/main/java/com/xrluo/SingletonTest1.java
+++ 01_singleton_pattern/src/main/java/com/xrluo/SingletonTest1.java
@@ -1,5 +1,7 @@
 package com.xrluo;
 
+import java.util.Objects;
+
 /**
  * 单例设计模式(饿汉模式)
  */
@@ -9,18 +11,26 @@ public class SingletonTest1 {
         Singleton singleton1 = Singleton.getSingleton();
 
         System.out.println(singleton + "\n" + singleton1);
+
+        System.out.println(Objects.equals(singleton, singleton1));
     }
 }
 
-
+/**
+ * 优点: 方法简单,避免线程同步的风险
+ * 缺点: 最初就直接创建了,有可能实际运行的程序并没有使用到,造成内存浪费
+ */
 class Singleton {
+
+    // 2. 本类内部创建对象实例化
     private final static Singleton singleton = new Singleton();
 
-    // 禁止实例化
+    // 1.构造器私有化
     private Singleton() {
 
     }
 
+    // 3. 向外暴露一个静态方法
     public static Singleton getSingleton() {
         return singleton;
     }
